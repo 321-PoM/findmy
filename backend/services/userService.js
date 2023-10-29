82ee66df4b3e3114a2185aa508d4b8954bc11660
muhan-alan-li <alanli8519@gmail.com>
2023-10-29 14:44:13 -0700

    updated rscore to return avg rating and stdev

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -59,11 +59,22 @@ export const getUserReliabilityScore = async (userId) => {
                 status: 'verfied'
             }
         });
-        
-        let reliabilityScore = 0; // TODO: What is our logic??? I don't know... Me no English...
-        
-        return reliabilityScore;
+        if(verifiedPoisReported == 0) throw new Error("User has not reported any POIs");
 
+        // return stdev and avg rating of verified POIs
+        let avgRating;
+        let stDev;
+
+        const sumRating = poiRatings.reduce((sum, poi) => sum += poi['rating'], 0);
+        avgRating = sumRating / verifiedPoisReported;
+        
+        let sumDeviation = poiRatings
+            .map((poi) => Math.pow((poi['rating'] - avgRating), 2))
+            .reduce((sum, distance) => sum += distance, 0);
+
+        stDev = Math.pow((sumDeviation / verifiedPoisReported), 0.5);
+
+        return {avgPOIRating: avgRating, stdev: stDev};
     } catch (err) {
         throw err; 
     }
