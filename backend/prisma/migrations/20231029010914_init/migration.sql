c18ef8df7c3fbe0313f987fb92d02c7d1888ab15
Junsu An <anjjunsu@gmail.com>
2023-10-28 19:26:26 -0700

    Init `Prisma`

---
+++ backend/prisma/migrations/20231029010914_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `avatar` VARCHAR(255) NOT NULL,
+    `biography` VARCHAR(255) NOT NULL,
+    `reliabilityScore` INTEGER NOT NULL,
+    `premiumStatus` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Image` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(191) NOT NULL,
+    `poiId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `poi` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `latitude` VARCHAR(31) NOT NULL,
+    `longitudes` VARCHAR(31) NOT NULL,
+    `category` VARCHAR(255) NOT NULL,
+    `status` ENUM('unlisted', 'verfied', 'private') NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `ownerId` INTEGER NOT NULL,
+    `rating` INTEGER NOT NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Image` ADD CONSTRAINT `Image_poiId_fkey` FOREIGN KEY (`poiId`) REFERENCES `poi`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `poi` ADD CONSTRAINT `poi_ownerId_fkey` FOREIGN KEY (`ownerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
