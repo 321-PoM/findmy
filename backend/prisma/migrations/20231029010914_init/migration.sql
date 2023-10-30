a1516b8160fa65225cb12b3f545d042d3ce7f65d
muhan-alan-li <alanli8519@gmail.com>
2023-10-30 10:58:08 -0700

    updated friendship db schema

--- backend/prisma/migrations/20231029010914_init/migration.sql
+++ backend/prisma/migrations/20231029010914_init/migration.sql
@@ -55,8 +55,9 @@ CREATE TABLE `friendship` (
     `userId2` INTEGER NOT NULL,
     `status` ENUM('requested', 'accepted', 'rejected') NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
-    PRIMARY KEY (`userIdFrom`)
+    PRIMARY KEY (`friendshipId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
