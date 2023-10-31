69f86f44f6cc5d235fd9b1e57c3a2549af68b0f0
Junsu An <anjjunsu@gmail.com>
2023-10-30 22:31:25 -0700

    bad CICD...

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ export const getUser = async (req, res) => {
         const user = await userService.getUser(req.params.id);
         res.status(200).json(user);
     } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: error });
     }
 };
 
@@ -14,7 +14,7 @@ export const createUser = async (req, res) => {
         const user = await userService.createUser(req.body);
         res.status(201).json(user);
     } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: error });
     }
 };
 
@@ -23,7 +23,7 @@ export const updateUser = async (req, res) => {
         const user = await userService.updateUser(req.params.id, req.body);
         res.status(200).json(user);
     } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: error });
     }
 };
 
@@ -32,7 +32,7 @@ export const deleteUser = async (req, res) => {
         await userService.deleteUser(req.params.id);
         res.status(204).send();
     } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: error });
     }
 };
 
@@ -41,7 +41,7 @@ export const listUsers = async (req, res) => {
         const users = await userService.listUsers();
         res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: error });
     }
 };
 
@@ -50,6 +50,6 @@ export const getReliabilityScore = async (req, res) => {
         const score = await userService.getUserReliabilityScore(req.params.userId);
         res.status(200).json({ reliabilityScore: score });
     } catch (err) {
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: error });
     }
 };
