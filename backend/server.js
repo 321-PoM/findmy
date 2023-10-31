fd7b74ebe683738507c6b5f9442454c81cb38ce2
Ubuntu <azureuser@findmy-vm.hbzlxlz0utkefon5fsffgkkczc.xx.internal.cloudapp.net>
2023-10-31 00:37:56 +0000

    quick path fix

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import fs         from 'fs';
 import userRoutes from './routes/userRoutes.js';
 import poiRoutes  from './routes/poiRoutes.js';
 import marketRoutes from './routes/marketRoutes.js';
-import reviewRoutes from '/routes/reviewRoutes.js';
+import reviewRoutes from './routes/reviewRoutes.js';
 
 const app = express();
 app.use(express.json());
