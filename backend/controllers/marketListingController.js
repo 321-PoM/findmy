0a244c5fe66d2c6d873aad203e0f57b000833101
muhan-alan-li <alanli8519@gmail.com>
2023-10-31 11:19:37 -0700

    market module filled out

--- backend/controllers/marketListingController.js
+++ backend/controllers/marketListingController.js
@@ -1,27 +1,55 @@
 import * as marketListingService from '../services/marketListingService.js';
 
-export const create = async (req, res) => {
-    const listing = await marketListingService.create(req.body);
-    res.json(listing);
+export const createListing = async (req, res) => {
+    try{
+        const listing = await marketListingService.createListing(req.body.price, req.body.sellerId, req.body.poiId);
+        res.send(200).json(listing);
+    } catch (error) {
+        res.send(500).json({ message: error.message });
+    }
 };
 
 export const getOne = async (req, res) => {
-    const listing = await marketListingService.getOne(req.params.id);
-    res.json(listing);
+    try{
+        const listing = await marketListingService.getOne(req.param.listingId);
+        res.send(200).json(listing);
+    } catch (error) {
+        res.send(500).json({ message: error.message });
+    }
 };
 
 export const getAll = async (req, res) => {
-    const listings = await marketListingService.getAll();
-    res.json(listings);
+    try{
+        const listings = await marketListingService.getAll();
+        res.send(200).json(listings);
+    } catch (error) {
+        res.send(500).json({ message: error.message });
+    }
 };
 
-export const update = async (req, res) => {
-    const listing = await marketListingService.update(req.params.id, req.body);
-    res.json(listing);
+export const getUserListings = async (req, res) => {
+    try{
+        const listings = await marketListingService.getUserListings(req.param.userId);
+        res.send(200).json(listings);
+    } catch (error) {
+        res.send(500).json({ message: error.message });
+    }
+};
+
+export const updateListing = async (req, res) => {
+    try{
+        const listing = await marketListingService.updateListing(req.param.listingId, req.body.data);
+        res.send(200).json(listing);
+    } catch (error) {
+        res.send(500).json({ message: error.message });
+    }
 };
 
 export const deleteListing = async (req, res) => {
-    await marketListingService.deleteListing(req.params.id);
-    res.json({ message: 'Listing deleted successfully.' });
+    try{
+        const listing = await marketListingService.deleteListing(req.param.listingId);
+        res.send(200).json(listing);
+    } catch (error) {
+        res.send(500).json({ message: error.message });
+    }
 };
-
--- backend/services/marketListingService.js
+++ backend/services/marketListingService.js
@@ -2,22 +2,70 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export const create = async (data) => {
-    return prisma.marketListing.create({ data });
+export const createListing = async (price, sellerId, poiId) => {
+    return await prisma.marketListing.create({
+        data: {
+            price: price,
+            sellerId: sellerId,
+            poiId: poiId
+        },
+        select: { id: true },
+    });
 };
 
 export const getOne = async (id) => {
-    return prisma.marketListing.findUnique({ where: { id: Number(id) } });
+    return await prisma.marketListing.findUnique({
+        where: { id: Number(id) },
+        select: {
+            id: true,
+            price: true,
+            sellerId: true,
+            poiId: true,
+        } 
+    });
 };
 
 export const getAll = async () => {
-    return prisma.marketListing.findMany();
+    return await prisma.marketListing.findMany({
+        where: { isDeleted: false },
+        select: { 
+            id: true,
+            price: true,
+            sellerId: true,
+            poiId: true,
+        }
+    });
 };
 
-export const update = async (id, data) => {
-    return prisma.marketListing.update({ where: { id: Number(id) }, data });
+export const getUserListings = async (userId) => {
+    return await prisma.marketListing.findMany({
+        where: {
+            sellerId: userId,
+            isDeleted: false,
+        },
+        select: {
+            id: true,
+            price: true,
+            sellerId: true,
+            poiId: true,
+        }
+    });
+};
+
+export const updateListing = async (id, data) => {
+    return await prisma.marketListing.update({
+        where: { id: Number(id) }, 
+        data: data,
+    });
 };
 
 export const deleteListing = async (id) => {
-    return prisma.marketListing.delete({ where: { id: Number(id) } });
+    return prisma.marketListing.update({
+        where: { id: Number(id) },
+        data: { 
+            isActive: false,
+            isDeleted: true,
+        },
+        select: { id: true },
+    });
 };
