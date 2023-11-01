23d6151eae693f91134f7caf1dcd19836f862cda
Kevin Zhao <kevinzhao272@gmail.com>
2023-11-01 14:41:10 -0700

    Added backend integration for getMyPOIs for ProfileFragment

--- frontend/app/src/main/java/com/example/findmy/ui/profile/ProfileFragment.java
+++ frontend/app/src/main/java/com/example/findmy/ui/profile/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,23 +15,35 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.findmy.model.POI;
+import com.example.findmy.network.FindMyService;
+import com.example.findmy.network.FindMyServiceViewModel;
 import com.example.findmy.ui.HomeActivity;
 import com.example.findmy.databinding.FragmentProfileBinding;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class ProfileFragment extends Fragment {
 
     private final String TAG = "ProfileFragment";
 
+    private FindMyService findMyService;
+
     private FragmentProfileBinding binding;
+    private ArrayList<POI> myPOIList;
+    private MyPOIListAdapter mPOIAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ProfileViewModel profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        findMyService = new ViewModelProvider(requireActivity()).get(FindMyServiceViewModel.class).getFindMyService();
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -44,7 +57,7 @@ public class ProfileFragment extends Fragment {
         });
 
         // setup myPOI list
-        ArrayList<POI> myPOIList = new ArrayList<POI>();
+        retrieveMyPOIs();
 
         POI testPOI = POI.testPOI;
         myPOIList.add(testPOI);
@@ -52,12 +65,35 @@ public class ProfileFragment extends Fragment {
         // TODO: update with live location
         LatLng currentLatLng = new LatLng(0.0, 0.0);
         RecyclerView myPOIRecycler = binding.myPOIRecycler;
-        MyPOIListAdapter mAdapter = new MyPOIListAdapter(requireActivity(), myPOIList, currentLatLng);
+        mPOIAdapter = new MyPOIListAdapter(requireActivity(), myPOIList, currentLatLng);
 
-        myPOIRecycler.setAdapter(mAdapter);
+        myPOIRecycler.setAdapter(mPOIAdapter);
         myPOIRecycler.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
         return root;
+    }
+
+    private void retrieveMyPOIs() {
+        myPOIList = new ArrayList<POI>();
+        Call<POI[]> call = findMyService.getPOIs();
+        call.enqueue(new Callback<POI[]>() {
+            @Override
+            public void onResponse(Call<POI[]> call, Response<POI[]> response) {
+                POI[] retrievedPOIs = response.body();
+                for (POI p : retrievedPOIs) {
+                    if (p.getCategory() == "myPOI") {
+                        myPOIList.add(p);
+                    }
+                }
+                mPOIAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<POI[]> call, Throwable t) {
+                Toast.makeText(requireContext(), "Error: Unable to retrieve myPOIs", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 
     @Override
