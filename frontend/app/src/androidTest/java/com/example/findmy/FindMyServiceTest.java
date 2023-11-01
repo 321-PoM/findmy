b3f9e9114847259f5f68ab6ea15cd41d9cf7c6af
jorido <djw0626123@gmail.com>
2023-11-01 16:21:13 -0700

    user api tests

--- frontend/app/src/androidTest/java/com/example/findmy/FindMyServiceTest.java
+++ frontend/app/src/androidTest/java/com/example/findmy/FindMyServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.findmy;
 
 import com.example.findmy.model.User;
+import com.example.findmy.model.UserRequest;
 import com.example.findmy.network.FindMyService;
 import com.example.findmy.network.FindMyServiceViewModel;
+import com.google.gson.Gson;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
@@ -14,32 +17,48 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class FindMyServiceTest {
-    FindMyServiceViewModel findMyServiceViewModel;
+    static FindMyServiceViewModel findMyServiceViewModel;
 
     // Used to synchronize request thread
     CountDownLatch signal;
 
-    @Before
-    public void initialize() {
-        signal = new CountDownLatch(1);
+    public void printUser(User[] users) {
+        if (users == null) {
+            System.out.println("NULL response");
+            return;
+        }
+        for (User user: users) {
+            Gson gson = new Gson();
+            String json = gson.toJson(user); //convert
+            System.out.println("User: \n" + json);
+        }
+
     }
-    @Test
-    public void testUser() throws InterruptedException {
 
-
+    @BeforeClass
+    public static void findmy() {
         findMyServiceViewModel = new FindMyServiceViewModel();
 
         findMyServiceViewModel.initFindMyService();
+    }
 
+    @Before
+    public void initialize() {
+        signal = new CountDownLatch(1);
+
+    }
+
+    // user
+    @Test
+    public void testCreateUser() throws InterruptedException {
         FindMyService findMyService = findMyServiceViewModel.getFindMyService();
 
-        System.out.println("Create User");
-
-        Call<User> call = findMyService.createUser(User.testUser);
+        Call<User> call = findMyService.createUser(UserRequest.testUserRequest);
         call.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-                System.out.println("Response Body: " + response.body());
+                User[] res = {response.body()};
+                printUser(res);
 
                 signal.countDown();
             }
@@ -51,14 +70,17 @@ public class FindMyServiceTest {
                 signal.countDown();
             }
         });
+        // wait for response to call signal.countDown() before terminating test;
+        signal.await();
+    }
 
-        System.out.println("-------------------------------");
-        System.out.println("Get all users");
-
+    @Test
+    public void testGetUsers() throws InterruptedException {
+        FindMyService findMyService = findMyServiceViewModel.getFindMyService();
         findMyService.getUsers().enqueue(new Callback<User[]>() {
             @Override
             public void onResponse(Call<User[]> call, Response<User[]> response) {
-                System.out.println("Response Body: " + response.body());
+                printUser(response.body());
 
                 signal.countDown();
             }
@@ -70,67 +92,125 @@ public class FindMyServiceTest {
                 signal.countDown();
             }
         });
-
-        System.out.println("-------------------------------");
-        System.out.println("Get test user");
-
-        findMyService.getUser(1).enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                System.out.println("Response Body: " + response.body());
-
-                signal.countDown();
-            }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                System.out.println("Failed");
-
-                signal.countDown();
-            }
-        });
-
-        System.out.println("-------------------------------");
-        System.out.println("Get user by email");
-
-        findMyService.getUserByEmail("kk@example.com").enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                System.out.println("Response Body: " + response.body());
-
-                signal.countDown();
-            }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                System.out.println("Failed");
-
-                signal.countDown();
-            }
-        });
-
-        System.out.println("-------------------------------");
-        System.out.println("Get user by email");
-
-        findMyService.getUserByEmail("kk@example.com").enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                System.out.println("Response Body: " + response.body());
-
-                signal.countDown();
-            }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                System.out.println("Failed");
-
-                signal.countDown();
-            }
-        });
-
-
-
         // wait for response to call signal.countDown() before terminating test;
         signal.await();
     }
+
+    @Test
+    public void testGetUserByEmail() throws InterruptedException {
+        FindMyService findMyService = findMyServiceViewModel.getFindMyService();
+
+        findMyService.getUserByEmail("kk@example.com").enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User[] res = {response.body()};
+                printUser(res);
+
+                signal.countDown();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                System.out.println("Failed");
+
+                signal.countDown();
+            }
+        });
+        // wait for response to call signal.countDown() before terminating test;
+        signal.await();
+    }
+
+    @Test
+    public void testGetUserById() throws InterruptedException {
+        FindMyService findMyService = findMyServiceViewModel.getFindMyService();
+        findMyService.getUser(1).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User[] res = {response.body()};
+                printUser(res);
+
+                signal.countDown();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                System.out.println("Failed");
+
+                signal.countDown();
+            }
+        });
+        // wait for response to call signal.countDown() before terminating test;
+        signal.await();
+    }
+
+    @Test
+    public void testGetUserReliabilityScore() throws InterruptedException {
+        FindMyService findMyService = findMyServiceViewModel.getFindMyService();
+        findMyService.getUserReliabilityScore(1).enqueue(new Callback<Integer>() {
+            @Override
+            public void onResponse(Call<Integer> call, Response<Integer> response) {
+                System.out.println("Response Body: " + response.body());
+
+                signal.countDown();
+            }
+
+            @Override
+            public void onFailure(Call<Integer> call, Throwable t) {
+                System.out.println("Failed");
+
+                signal.countDown();
+            }
+        });
+        // wait for response to call signal.countDown() before terminating test;
+        signal.await();
+    }
+
+    @Test
+    public void testUpdateUser() throws InterruptedException {
+        FindMyService findMyService = findMyServiceViewModel.getFindMyService();
+        findMyService.updateUser(2, UserRequest.testUserRequest).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User[] res = {response.body()};
+                printUser(res);
+
+                signal.countDown();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                System.out.println("Failed");
+
+                signal.countDown();
+            }
+        });
+        // wait for response to call signal.countDown() before terminating test;
+        signal.await();
+    }
+
+    @Test
+    public void testDeleteUser() throws InterruptedException {
+        FindMyService findMyService = findMyServiceViewModel.getFindMyService();
+        findMyService.deleteUser(2).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User[] res = {response.body()};
+                printUser(res);
+
+                signal.countDown();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                System.out.println("Failed");
+
+                signal.countDown();
+            }
+        });
+        // wait for response to call signal.countDown() before terminating test;
+        signal.await();
+    }
+
+    //POI
+
 }
