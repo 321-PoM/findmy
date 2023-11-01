a8c0a1e8c0da8a7153f0ba8120af7bde21edc828
Kevin Zhao <kevinzhao272@gmail.com>
2023-11-01 03:12:52 -0700

    Added client for RetrofitClient

--- frontend/app/src/main/java/com/example/findmy/network/RetrofitClient.java
+++ frontend/app/src/main/java/com/example/findmy/network/RetrofitClient.java
@@ -1,14 +1,20 @@
 package com.example.findmy.network;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit nodeRetrofit;
-    private static final String NODE_URL = "http://3.232.242.38:8081/";
+
+    private static final HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    private static final OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+    private static final String NODE_URL = "https://findastar.westus2.cloudapp.azure.com/";
     public static Retrofit getNodeRetrofitInstance() {
         if (nodeRetrofit == null) {
             nodeRetrofit = new Retrofit.Builder()
                     .baseUrl(NODE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
