ebeb07c12fd42bce508e552c652dad9292d01e5c
Kevin Zhao <kevinzhao272@gmail.com>
2023-11-01 19:11:29 -0700

    Refactored AddPOI to use BottomSheet

--- frontend/app/src/main/java/com/example/findmy/model/POIRequest.java
+++ frontend/app/src/main/java/com/example/findmy/model/POIRequest.java
@@ -6,7 +6,7 @@ public class POIRequest {
     @SerializedName("latitude")
     private double latitude;
 
-    @SerializedName("longitude")
+    @SerializedName("longitudes")
     private double longitude;
 
     @SerializedName("category")
