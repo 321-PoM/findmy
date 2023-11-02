d2db7ca9fd8a5da13e27ad985708e111589a5f7f
John Do <84487643+jorido@users.noreply.github.com>
2023-11-02 15:34:47 -0700

    getFriendship uses friendshipId extracted from url param

--- backend/routes/friendRoutes.js
+++ backend/routes/friendRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/friends/:userId', friendController.listFriends);  
 router.get('/friends/:userId/received', friendController.listRequestsReceived);
 router.get('/friends/:userId/sent', friendController.listRequestsSent);
-router.get('/friend', friendController.getFriendship);
+router.get('/friend/:friendshipId', friendController.getFriendship);
 router.post('/friend', friendController.createFriendship);
 router.put('/friend', friendController.handleFriendRequest);
 router.delete('/friend', friendController.deleteFriendship);
