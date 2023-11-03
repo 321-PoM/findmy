70fad7496cbe2f91c9d0eea1e33ddba07b4664d2
Kevin Zhao <kevinzhao272@gmail.com>
2023-11-02 23:16:26 -0700

    Friend request functionality in progress

--- frontend/app/src/main/java/com/example/findmy/ui/friends/FriendsFragment.java
+++ frontend/app/src/main/java/com/example/findmy/ui/friends/FriendsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,12 +13,19 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.findmy.model.Friendship;
+import com.example.findmy.model.FriendshipRequest;
 import com.example.findmy.model.User;
 import com.example.findmy.databinding.FragmentFriendsBinding;
 import com.example.findmy.network.FindMyService;
 import com.example.findmy.network.FindMyServiceViewModel;
+import com.example.findmy.ui.HomeActivity;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class FriendsFragment extends Fragment {
@@ -25,6 +34,15 @@ public class FriendsFragment extends Fragment {
     private FindMyService findMyService;
     private FriendsAdapter friendsAdapter;
     private ArrayList<User> friendsArray;
+    private Button addButton;
+
+    private final View.OnClickListener addFriendListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            initiateFriendRequest(getFriendTextField());
+        }
+    };
+    private EditText friendTextField;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,17 +52,74 @@ public class FriendsFragment extends Fragment {
         findMyService = new ViewModelProvider(requireActivity()).get(FindMyServiceViewModel.class).getFindMyService();
 
         binding = FragmentFriendsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
         retrieveFriends();
+        setupAddButton(binding);
 
+        setupRecycler(binding);
+        setupFriendTextField(binding);
+
+        View root = binding.getRoot();
+        return root;
+    }
+
+    private void addFriendWithID(int friendID) {
+        User currentUser = ((HomeActivity) requireActivity()).currentUser;
+        FriendshipRequest request = new FriendshipRequest(currentUser.getId(), friendID);
+        findMyService.createFriendship(request).enqueue(
+                new Callback<Friendship>() {
+                    @Override
+                    public void onResponse(Call<Friendship> call, Response<Friendship> response) {
+                        if(!response.isSuccessful()) {
+                            findMyService.showErrorToast(requireContext());
+                        }
+                        // TODO: update UI?
+                    }
+
+                    @Override
+                    public void onFailure(Call<Friendship> call, Throwable t) {
+                        findMyService.showErrorToast(requireContext());
+                    }
+                }
+        );
+    }
+
+    private void initiateFriendRequest(String email) {
+        findMyService.getUserByEmail(email).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if(!response.isSuccessful()) {
+                    findMyService.showErrorToast(requireContext());
+                }
+                addFriendWithID(response.body().getId());
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                findMyService.showErrorToast(requireContext());
+            }
+        });
+    }
+
+    private void setupFriendTextField(FragmentFriendsBinding binding) {
+        friendTextField = binding.addFriendField;
+    }
+
+    private String getFriendTextField() {
+        return String.valueOf(friendTextField.getText());
+    }
+
+    private void setupAddButton(FragmentFriendsBinding binding) {
+        addButton = binding.addFriendButton;
+        addButton.setOnClickListener(addFriendListener);
+    }
+
+    private void setupRecycler(FragmentFriendsBinding binding) {
         RecyclerView friendsRecycler = binding.friendsRecycler;
         friendsAdapter = new FriendsAdapter(requireActivity(), friendsArray);
 
         friendsRecycler.setAdapter(friendsAdapter);
         friendsRecycler.setLayoutManager(new LinearLayoutManager(this.getContext()));
-
-        return root;
     }
 
     private void retrieveFriends() {
