7f50ea2837816bb182fa2f05ec7d48d73fbc079c
muhan-alan-li <alanli8519@gmail.com>
2023-11-03 12:09:28 -0700

    fixed typo

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -23,8 +23,8 @@ export const getReview = async (req, res) => {
 export const createReview = async (req, res) => {
     try {
         const review = await reviewService.createReview(req.body.poiId, req.body.userId, req.body.rating, req.body.desc);
-        const newRating = await calcPoiRating(poiId);
-        const updated = await updatePoi(poiId, {rating: newRating});
+        const newRating = await calcPoiRating(req.body.poiId);
+        const updated = await updatePoi(req.body.poiId, {rating: newRating});
         res.status(200).json(review);
     } catch (error) {
         res.status(500).json({ message: error.message });
