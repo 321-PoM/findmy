638a1323f7838f8971838333d7bd9804682c4474
Muhan Li <alanli8519@gmail.com>
2023-11-03 16:55:48 -0700

    meanDist log

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -107,6 +107,7 @@ export const getUserReliabilityScore = async (userId) => {
         }
         let sumDist = reviewsByUser.reduce((sum, dist) => sum += dist, 0);
         let meanDist = (sumDist === 0) ? 0 : parseInt(sumDist / totalReviews);
+        console.log(`meanDist: ${meanDist}`);
         return 100 - meanDist;
     } catch (err) {
         throw err; 
