621312e1cda31114010a2d5cc0e7e053396f367a
muhan-alan-li <alanli8519@gmail.com>
2023-11-17 01:18:35 -0800

    marketlisting and friends

---
+++ backend/tests/marketListing.test.js
@@ -0,0 +1,167 @@
+// interface POST host/marketlisting
+describe("create market listing", () => {
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("valid body", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("invalid body", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+});
+
+// interface GET host/marketListing/:listingId
+describe("get one market listing", () => {
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("valid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("invalid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+});
+
+// interface GET host/marketListings
+describe("get all market listings", () => {
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("get all test", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+});
+
+// interface GET host/marketListings/:userId
+describe("get all listings of a user", () => {
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("valid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("invalid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+});
+
+// interface GET host/marketListing/poi/:poiId
+describe("get all listings of a poi", () => {
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("valid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("invalid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+});
+
+// interface PUT host/marketListing/:listingId
+describe("update listing", () => {
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("valid id, valid body", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("invalid id, valid body", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("valid id, invalid body", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+});
+
+// interface DELETE host/marketListing/:listingId
+describe("delete listing", () => {
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("valid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+
+    // input
+    // expected status code
+    // expected behaviour
+    // expected output
+    test("invalid id", async () => {
+        const res = await app.post();
+        expect(res.status).toStrictEqual();
+        expect();
+    });
+});
