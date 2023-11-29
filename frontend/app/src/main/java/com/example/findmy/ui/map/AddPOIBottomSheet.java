0dff48e938bbd392dbc36aa2370637f594cdcba3
Kevin Zhao <kevinzhao272@gmail.com>
2023-11-28 22:19:32 -0800

    Added some placeholder code

--- frontend/app/src/main/java/com/example/findmy/ui/map/AddPOIBottomSheet.java
+++ frontend/app/src/main/java/com/example/findmy/ui/map/AddPOIBottomSheet.java
@@ -27,6 +27,7 @@ import com.example.findmy.ui.HomeActivity;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.io.File;
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -41,7 +42,7 @@ public class AddPOIBottomSheet extends BottomSheetDialogFragment implements  Ada
 
     Location currLocation;
 
-    private View.OnClickListener submitNewPOIListener = new View.OnClickListener() {
+    private final View.OnClickListener submitNewPOIListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
@@ -51,7 +52,14 @@ public class AddPOIBottomSheet extends BottomSheetDialogFragment implements  Ada
 
             int currentUserId = ((HomeActivity) requireActivity()).getCurrentUserId();
 
-            POIRequest newPOI = new POIRequest(currLocation.getLatitude(), currLocation.getLongitude(), poiType, "verified", poiName, currentUserId, rating, 0, false, new File("error"));
+            File poiImageFile;
+            try {
+                poiImageFile = File.createTempFile("poi", ".jpeg");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            POIRequest newPOI = new POIRequest(currLocation.getLatitude(), currLocation.getLongitude(), poiType, "verified", poiName, currentUserId, rating, 0, false, poiImageFile);
 
             findMyService.createPOI(newPOI).enqueue(
                     new Callback<POI>() {
