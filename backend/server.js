bb496ac4c2c95e71c9696fba22c9846cbb0306a4
Junsu An <anjjunsu@gmail.com>
2023-11-30 01:32:51 -0800

    Not listenig on ports when it's in test mode.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import express    from 'express';
-import http       from 'http';
-import https      from 'https';
-import fs         from 'fs';
+import express from 'express';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import userRoutes from './routes/userRoutes.js';
-import poiRoutes  from './routes/poiRoutes.js';
+import poiRoutes from './routes/poiRoutes.js';
 import marketListingRoutes from './routes/marketListingRoutes.js';
 import transactionRoutes from './routes/transactionRoute.js';
 import reviewRoutes from './routes/reviewRoutes.js';
@@ -15,15 +15,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-// API Routers
+/* Routers */
 app.use(userRoutes);
 app.use(poiRoutes);
 app.use(marketListingRoutes);
 app.use(transactionRoutes);
 app.use(reviewRoutes);
 app.use(friendRoutes);
-
-var key_cert_path = process.env.KEY_CERT_PATH
 
 var server_opt = {
     key: fs.readFileSync(process.env.KEY_PATH),
@@ -35,9 +33,16 @@ var ports = {
     https: process.env.TEST_ENV ? process.env.HTTPS_PORT : 443,
 }
 
-const httpServer = http.createServer(app).listen(ports.http);
-const httpsServer = https.createServer(server_opt, app).listen(ports.https);
-
-console.log(`Server running at http port:${ports.http}; https port:${ports.https}`);
+/*
+ * If testing, don't listen on ports.
+ * Github actions Jest test will fail if we try to listen on ports that are already in use.
+ * `TEST_ENV` is set in the `test/setup.mjs` file.
+ */
+let httpServer, httpsServer;
+if (!process.env.TEST_ENV) {
+    httpServer = http.createServer(app).listen(ports.http);
+    httpsServer = https.createServer(server_opt, app).listen(ports.https);
+    console.log(`Server running at http port:${ports.http}; https port:${ports.https}`);
+}
 
 export { httpServer, httpsServer, app };
