8681ed63ad6ddeed412e29f8080cc0e453b3e49c
Junsu An <anjjunsu@gmail.com>
2023-11-30 00:17:05 -0800

    Fix import `server.js:app` error.

--- backend/tests/review.test.js
+++ backend/tests/review.test.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { request } from 'supertest';
-import app from '../server.js';
+import { app } from '../server.js';
 
 // interface GET host/reviews/:searchby/:id
 describe("Search reviews by user or by poi", () => {
